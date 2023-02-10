72cd3a39f7041f81af23a60666ca1b4a0bf629fa
gregorymacat <gregorymacat@gmail.com>
2023-02-09 23:18:42 -0600

    Opening interview project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// Main page info
+  // Sec 1: airport idientifier (icao), 
+  // Sec 2: airport name, 
+  // Sec 3: available runways, 
+  // Sec 4: aiport lat/long
+  // Sec 5: current weather report
+    // 5.1: Temp (in Farenheit)
+    // 5.2: Relative Humidity (%)
+    // 5.4: Summary of cloud coverage (text string)
+      // 5.4.1: Greatest amount if any listed
+    // 5.5: Visibility (Statute Miles)
+    // Wind Speed (MPH)
+    // Wind Direction (cardinal to secondary intercardinal precision)
+
+const getAirportData = () => {
+  
+}
+
+function App() {
+
+  return (
+    <div>
+      <h1>ForeFlight Interview</h1>
+    </div>
+  );
+}
+
+export default App;
